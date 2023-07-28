556edc6c714425961179139f6bfc53ca6da25b7f
macbaansuk <macbaansuk@gmail.com>
2023-07-28 12:39:12 +0900

    ~p.200

--- Chapter03/src/test/java/com/rubypaper/biz/client/EmployeeServiceClient.java
+++ Chapter03/src/test/java/com/rubypaper/biz/client/EmployeeServiceClient.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
-import javax.persistence.FlushModeType;
 import javax.persistence.Persistence;
 
 import com.rubypaper.biz.domain.Employee;
@@ -17,13 +16,8 @@ public class EmployeeServiceClient {
 		//엔티티 매니저 팩토리 생성
 		EntityManagerFactory emf = //클라이언트 마다 생성
 				Persistence.createEntityManagerFactory("Chapter03");
-		
 		//엔티티 매니저 생성
 		EntityManager em = emf.createEntityManager();
-		
-		//플러시 모드 설정
-		em.setFlushMode(FlushModeType.COMMIT);
-		
 		//엔티티 트랜잭션 생성
 		EntityTransaction tx = em.getTransaction();
 		
@@ -39,21 +33,14 @@ public class EmployeeServiceClient {
 			//트랜잭션 종료(COMMIT)
 			tx.commit(); 
 			
-			//모든 엔티티를 분리 상태로 전환시킨다.
-			em.clear();
+			for(int i = 0; i < 30; i++) {
+				Thread.sleep(1000);
+				System.out.println("다른 사용자가 데이터 수정중 ... " + i);
+			}
 			
-			//직원 엔티티 이름 수정
-			tx.begin();
-			employee.setName("뚤리");
-			Employee mergedEmp = em.merge(employee); //mergedEmp만 관리상태
-			tx.commit(); 
-			
-			//광리 상태 여부 확인
-			System.out.println("employee 관리 : " + em.contains(employee));
-			System.out.println("mergedEmp 관리 : " +em.contains(mergedEmp));
-			
-			
-			
+			//엔티티 REFRESH
+			em.refresh(employee);
+			System.out.println("갱신된 직우너 정보 : " + employee.toString());
 			
 		} catch (Exception e) {
 			e.printStackTrace();
